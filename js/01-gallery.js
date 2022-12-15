3d18662e1034756e19993bafcd9ee50b48c320e0
Transcendence <baydindenys@ukr.net>
2022-12-15 22:33:47 +0200

    upd

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,12 +1,15 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+
+
 const list = document.querySelector('.gallery')
 
-const markup = images
-	.map(galleryItems => `<li><img class="picture" src=${image.url} width = '400' alt='${image.alt}'></img></li>`)
-	.join('')
+const markup = galleryItems.map(({preview, description}) => {
+  return `
+  <li><img class="picture" src=${preview} width = '400' alt='${description}'></img></li>`;
+});
 
-  list.insertAdjacentHTML('beforeend', markup)
-    // const img = document.createElement('img')
+
+
+
   
-const pictures = document.querySelectorAll('.picture')

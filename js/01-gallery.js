640fb7dd6b6bbd7e306f7d098cc74c3efd6a9c85
Transcendence <109620661+Trancendence@users.noreply.github.com>
2022-12-15 18:31:36 +0200

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,12 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const list = document.querySelector('.gallery')
 
-console.log(galleryItems);
+const markup = images
+	.map(galleryItems => `<li><img class="picture" src=${image.url} width = '400' alt='${image.alt}'></img></li>`)
+	.join('')
+
+  list.insertAdjacentHTML('beforeend', markup)
+    // const img = document.createElement('img')
+  
+const pictures = document.querySelectorAll('.picture')

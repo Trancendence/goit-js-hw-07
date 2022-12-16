86cba60ebf1f67de36cf5d71c602c76b8db7d278
Transcendence <109620661+Trancendence@users.noreply.github.com>
2022-12-16 13:39:05 +0200

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,14 +2,44 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 
-const list = document.querySelector('.gallery')
+const imagesContainer = document.querySelector('.gallery')
+const imagesMarkup = createPictureGallery(galleryItems);
+imagesContainer.insertAdjacentHTML("beforeend", imagesMarkup);
 
-const markup = galleryItems.map(({preview, description}) => {
-  return `
-  <li><img class="picture" src=${preview} width = '400' alt='${description}'></img></li>`;
+function createPictureGallery(galleryItems) {
+  return galleryItems
+  .map(({preview, original, description}) => {
+  return `div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+  })
+  .join("");
+}
+
+imagesContainer.addEventListener("click", (eve) => {
+  eve.preventDefault();
+
+  // if not a picture - exit
+
+  if (eve.target.nodeName !== "IMG") {
+    return;
+  }
+
+  // if picture - show
+
+  const urlBigImage = eve.target.getAttribute("data-source");
+
+  const instance = bacisLightbox.create(`
+  <img src="${urlBigImage}" width="800" heigth="600">
+  `);
+
+  instance.show();
+
 });
-
-
-
-
-  
